921fe99e1534755c23e9fe570963cb7c057e0fe8
DeathFull <contact@deathfull.net>
2024-02-27 14:45:04 +0100

    add: Header visual

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,27 @@
-import { Nav } from "react-bootstrap";
+import { Button, Container, Form, Nav, Navbar } from "react-bootstrap";
 
 function Header() {
-  return <Nav></Nav>;
+  return (
+    <Navbar bg="dark" data-bs-theme="dark" className={"p-4 mb-4"}>
+      <Container fluid>
+        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#features">Features</Nav.Link>
+          <Nav.Link href="#pricing">Pricing</Nav.Link>
+        </Nav>
+        <Form className="d-flex">
+          <Form.Control
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default Header;
