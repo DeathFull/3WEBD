e143ae08bedf10be5684bffbf478183c112b385b
DeathFull <contact@deathfull.net>
2024-02-28 13:39:31 +0100

    add: home component in router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainPage from "./components/MainPage.tsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./query/ApiQuery.tsx";
+import Home from "./components/Home.tsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainPage />,
     children: [
+      {
+        path: "",
+        element: <Home />,
+      },
       {
         path: "test",
         element: <h1>Test</h1>,
@@ -16,7 +23,11 @@ const router = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
